3064e50d4aa753649280007d3fc52d75a653a7e0
katia <estambolieva@gmail.com>
2020-08-31 10:57:13 +0300

    added entity ruler to manually annotate Named Entities - e.g. protagonists' names

--- code/coreference_resoluton.py
+++ code/coreference_resoluton.py
@@ -4,6 +4,31 @@ import spacy
 import neuralcoref
 
 # load needed language resources
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en')
 neuralcoref.add_to_pipe(nlp)
 
+# load Spacy's entity ruler which allows for manual annotation of Named Entities
+ruler = nlp.create_pipe("entity_ruler")
+
+# load the Pride & Prejudice data
+excerpts = pd.read_excel('../data/Sample_Paragraphs.xlsx', 'Sheet1')
+text = excerpts.loc[8].Paragraph
+protagonist = excerpts.loc[8].Character
+
+# Count how many times the name of the character to be re-genders occurs in the texts
+### we do not split the character name of Mrs. Bennet
+
+### see if spacy removes words which are not named entities, such as `Mrs.` and `the`
+doc = nlp(protagonist)
+print('--------')
+found_entities = doc.ents != ()
+if found_entities:
+    for ent in doc.ents:
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
+    print('--------')
+else:
+    ruler.add_patterns([{"label": "PERSON", "pattern": protagonist}])
+    nlp.add_pipe(ruler)
+    doc = nlp(protagonist)
+    for ent in doc.ents:
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
