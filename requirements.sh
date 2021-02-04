b3359ea9c06c5ebe21966c395af329b33985a390
Ekaterina Stambolieva <ekaterina.stambolieva@cybele.space>
2021-02-04 14:48:26 +0200

    creates the folder output automatically

--- requirements.sh
+++ requirements.sh
@@ -4,6 +4,11 @@
 # tutorials on how to install for Windows, Linux & Mac - https://www.atlassian.com/git/tutorials/install-git
 #######################################
 
+## Detect the folder in which this file, requirements.sh, is located
+SCRIPT=$(readlink -f "$0") # Absolute path to this script
+SCRIPTPATH=$(dirname "$SCRIPT") # Absolute path this script is in
+echo "$SCRIPTPATH"
+
 ## Detect Operating System
 ## TODO: only works for Linux and Mac
 OS=$(uname -s)
@@ -11,15 +16,15 @@ OS=$(uname -s)
 ## a. Detected a linux system
 if [ $OS = "Linux" ]
 then
-  echo '###Detected a Linux system..'
+  echo 'Detected a Linux system.. Following installation instructions for Linux systems.'
 
   ## Update packages and Upgrade system
-  echo '###Updating packages..'
+  echo 'Updating packages..'
   sudo apt-get update -y
   sudo apt autoremove
 
   ## Git ##
-  echo '###Installing Git..'
+  echo 'Installing Git..'
   sudo apt-get install git -y
 
   git --version
@@ -29,14 +34,17 @@ fi
 ## b. Detected a Mac
 if [ $OS = "Darwin" ]
 then
-  echo '###Detected a Mac..'
+  echo 'Detected a Mac..'
 
   ## Update packages and Upgrade system
-  echo '###Updating packages..'
+  echo 'Updating packages..'
   brew update && brew upgrade
 
-  echo '###Installing Git..'
+  echo 'Installing Git..'
   brew install git
 
   git --version
 fi
+
+## create the output folder in which we will write the files with regendered texts
+mkdir $SCRIPTPATH/output
